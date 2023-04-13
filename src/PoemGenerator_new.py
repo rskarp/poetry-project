02de7b1fa71cd81095dc4fbb38876bf723c907bf
rskarp <rkarp1997@gmail.com>
2023-04-13 00:35:49 -0400

    new poems

--- src/PoemGenerator_new.py
+++ src/PoemGenerator_new.py
@@ -2,16 +2,20 @@ import spacy
 import datamuse
 import os
 import time
+import datetime
 import concurrent.futures
 from random import sample
+from string import punctuation
 
-BEGIN_TOKEN = '#BEGIN'
-END_TOKEN = '#END'
+BEGIN_AUTHOR_TOKEN = '#BEGIN_AUTHOR#'
+END_AUTHOR_TOKEN = '#END_AUTHOR#'
+BEGIN_TOKEN = '#BEGIN\n'
+END_TOKEN = '#END\n'
 REPLACE_TOKEN = '#REPLACE'
 dm = datamuse.Datamuse()
+nlp = spacy.load("en_core_web_md")
 
 def get_tokens(text):
-    nlp = spacy.load("en_core_web_md")
     doc = nlp(text)
     tokens = [token for token in doc]
     content_tokens = [token for token in doc if token.pos_ in ['NOUN', 'VERB', 'ADJ', 'ADV']]
@@ -47,11 +51,12 @@ def save_poem(wordList, filename):
 
 def get_candidates(token, replacement_types, max_results = 50):
     # print(f'Replacing {token.text}...')
-    ml = token.text if 'ml' in replacement_types else None
-    sp = f'//{token.text}' if 'ana' in replacement_types else token.text if 'sp' in replacement_types else None
-    rel_trg = token.text if 'rel_trg' in replacement_types else None
-    rel_cns = token.text if 'rel_cns' in replacement_types else None
-    rel_hom = token.text if 'rel_hom' in replacement_types else None
+    text = token.text.strip(punctuation)
+    ml = text if 'ml' in replacement_types else None
+    sp = f'//{text}' if 'ana' in replacement_types else text if 'sp' in replacement_types else None
+    rel_trg = text if 'rel_trg' in replacement_types else None
+    rel_cns = text if 'rel_cns' in replacement_types else None
+    rel_hom = text if 'rel_hom' in replacement_types else None
     
     # Get words using Datamuse API
     options = dm.words(md='p', ml=ml, sp=sp, rel_trg=rel_trg, rel_cns=rel_cns, rel_hom=rel_hom, max=max_results)
@@ -125,20 +130,21 @@ def replace_random_words(filename, percent_to_replace, replacement_types=['ml'],
         
         out_words.append(f'{newWord}{token.whitespace_}')
 
-    outFilename = filename.replace('.txt',f'_{percent_to_replace}_{"_".join(replacement_types)}_{time.time()}.txt')
+    curDatetime = datetime.datetime.now().strftime( '%Y-%m-%d-%H.%M.%S.%f')
+    outFilename = filename.replace('.txt',f'_{percent_to_replace}_{"_".join(replacement_types)}_{curDatetime}.txt')
     print(outFilename)
     save_poem(out_words, outFilename)
     
     end = time.time()
     print(f'Experiment time elapsed ({outFilename}): {end-start} seconds ({(end-start)/60} minutes)')
 
-def run_experiments():
+def run_experiments_on_poem():
     # replace_marked_words('stein-tender-buttons.txt')
     # replace_marked_words('shakespeare-translations.txt')
 
     filename = 'modernist-poems.txt'
-    percent_options = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
-    replacement_options = [['ml'], ['sp'], ['rel_trg'], ['rel_cns'], ['rel_hom'], ['ml','sp'], ['ana'], ['ml','ana']]
+    # percent_options = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100]
+    # replacement_options = [['ml'], ['sp'], ['rel_trg'], ['rel_cns'], ['rel_hom'], ['ml','sp'], ['ana'], ['ml','ana']]
     
     percent_options = [100]
     replacement_options = [['ml'],['rel_trg']]
@@ -154,5 +160,84 @@ def run_experiments():
 
     print(f'Total time elapsed: {end-start} seconds ({(end-start)/60} minutes)')
 
+# New 4/12
+def replace_all_random_words(authorName, idx, tokens, content_tokens, replacement_types=['ml'], max_options=50):
+    start = time.time()
+
+    out_words = []
+    readingPoemContent = False
+    for i, token in enumerate(tokens):
+        if i >= 2 and tokens[i-1].text == 'BEGIN' and tokens[i-2].text == '#':
+            readingPoemContent = True
+        elif i >= 2 and tokens[i-1].text == 'END' and tokens[i-2].text == '#':
+            readingPoemContent = False
+        newWord = token.text
+        if readingPoemContent and token in content_tokens:
+            options = get_candidates(token, replacement_types, max_options)
+            if len(options) <= 1:
+                print(f'Num candidates: {len(options)}, word: {token.text.strip(punctuation)}, type: {",".join(replacement_types)}')
+            if len(options) > 0:
+                chosen = sample(options, 1)
+                newWord = f'{chosen[0]["word"]}[#ORIGINAL_{token.text}]'
+            else:
+                newWord = f'{token.text}[#ORIGINAL_{token.text}]'
+        
+        out_words.append(f'{newWord}{token.whitespace_}')
+
+    curDatetime = datetime.datetime.now().strftime( '%Y-%m-%d-%H.%M.%S.%f')
+    outFilename = f'{authorName}-{idx}_100_{"_".join(replacement_types)}_{curDatetime}.txt'
+    print(outFilename)
+    save_poem(out_words, outFilename)
+    
+    end = time.time()
+    print( f'\t\tPoem variation time elapsed ({outFilename}): {end-start} seconds ({(end-start)/60} minutes)')
+
+def run_author_poem(authorName, poem, idx):
+    print(f'{authorName} {idx}')
+    start = time.time()
+    tokens, content_tokens = get_tokens(poem)
+    replacement_options = [['ml'],['rel_trg']]
+    args = [(authorName, idx, tokens, content_tokens, repl) for repl in replacement_options]
+
+    with concurrent.futures.ThreadPoolExecutor() as executor3:
+        executor3.map(lambda a: replace_all_random_words(*a), args)
+        # for res in pool:
+        #     print(res)
+
+    end = time.time()
+
+    print(f'\tDone {authorName} poem {idx} variations: {end-start} seconds ({(end-start)/60} minutes)')
+
+def run_author_poems(authorText):
+    start = time.time()
+    poems = authorText.split(BEGIN_TOKEN)
+    authorName = poems[0].strip()
+    args = [(authorName, f'{BEGIN_TOKEN}\n{poems[i]}', i) for i in range(1,len(poems[1:]))]
+    
+    # print(f'Num {authorName} poems: {len(args)}')
+    with concurrent.futures.ThreadPoolExecutor() as executor2:
+        executor2.map(lambda a: run_author_poem(*a), args)
+        # for res in pool:
+        #     print(res)
+
+    end = time.time()
+    print(f'DONE {authorName}: {end-start} seconds ({(end-start)/60} minutes)')
+
+def run_experiments_on_poems():
+    start = time.time()
+    print('Reading file...')
+    filename = 'modernists_ascii.txt'
+    data = read_poem(filename)
+    authors = data.split(BEGIN_AUTHOR_TOKEN)
+    
+    print('Running poems...')
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        pool = executor.map(run_author_poems, authors[1:])
+        # for res in pool:
+        #     print(res)
+    end = time.time()
+    print(f'DONE WHOLE FILE: {end-start} seconds ({(end-start)/60} minutes)')
+
 if __name__ == '__main__':
-    run_experiments()
+    # run_experiments_on_poem()
+    run_experiments_on_poems()
